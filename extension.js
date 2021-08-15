bfc8831591698b50c2773b85809e6d1488bf0664
olmedoluis <olmedoluis@gmail.com>
2021-08-15 16:08:30 -0300

    refactor: remove deprecated comments

--- extension.js
+++ extension.js
@@ -1,27 +1,11 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "env-viewer" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('env-viewer.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from env-viewer!');
+		vscode.window.showInformationMessage('Interpretating...');
 	});
 
 	context.subscriptions.push(disposable);
