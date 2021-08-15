0631e4788349549bd5e9bdae113a16e11dcaceab
olmedoluis <olmedoluis@gmail.com>
2021-08-15 19:47:18 -0300

    feature: Gather information of file

--- extension.js
+++ extension.js
@@ -5,7 +5,18 @@ const vscode = require('vscode');
  */
 function activate(context) {
 	let interpretateCommand = vscode.commands.registerCommand('env-viewer.interpretateEnv', function () {
-		vscode.window.showInformationMessage('Interpretating...');
+		const panel = vscode.window.createWebviewPanel(
+			'editor',
+			'Env Editor',
+			vscode.ViewColumn.One
+		);
+		
+		panel.webview.html = getWebviewContent();
+
+		const text = vscode.window.activeTextEditor.document.getText();
+		console.log("text", text)
+
+		vscode.window.showInformationMessage('Interpretating...', text,"!");
 	});
 
 	context.subscriptions.push(interpretateCommand);
@@ -17,4 +28,23 @@ function deactivate() {}
 module.exports = {
 	activate,
 	deactivate
+}
+
+
+
+function getWebviewContent() {
+	return `
+	<!DOCTYPE html>
+
+	<html lang="en">
+	<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Env Editor</title>
+	</head>
+	<body>
+			<h1>Env Editor</h1>
+	</body>
+	</html>
+	`;
 }
