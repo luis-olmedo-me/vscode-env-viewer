27be2a5aebbb1a90ddcd8b68a67ac8baf5f40e10
olmedoluis <63429352+olmedoluis@users.noreply.github.com>
2021-08-29 02:13:48 -0300

    [FEAT] Interpretate files with command (#2)
    * feature: Gather information of file
    
    * feature: Show content in second window
    
    * feature: Use dot env parser
    
    * feature: Enable to use prettier
    
    * feature: Use helper to parse
    
    * feature:Separate env values
    
    * feature:Show how they are separated
    
    * feature:Show env values as inputs
    
    * refactor:Format html
    
    * feature:Show keys and values as a table
    
    * refactor:Remove unnecessary files
    
    Co-authored-by: olmedoluis <olmedoluis@gmail.com>

--- extension.js
+++ extension.js
@@ -1,20 +1,109 @@
-const vscode = require('vscode');
+const vscode = require('vscode')
+const { parse } = require('dotenv')
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	let interpretateCommand = vscode.commands.registerCommand('env-viewer.interpretateEnv', function () {
-		vscode.window.showInformationMessage('Interpretating...');
-	});
+  let interpretateCommand = vscode.commands.registerCommand(
+    'env-viewer.interpretateEnv',
+    function () {
+      const panel = vscode.window.createWebviewPanel(
+        'editor',
+        'Env Editor',
+        vscode.ViewColumn.Two
+      )
 
-	context.subscriptions.push(interpretateCommand);
+      const fileContent = vscode.window.activeTextEditor.document.getText()
+      panel.webview.html = getWebviewContent(fileContent)
+
+      vscode.window.showInformationMessage('Interpretating...')
+    }
+  )
+
+  context.subscriptions.push(interpretateCommand)
 }
 
 // this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
+  activate,
+  deactivate,
+}
+
+const envKeys = {
+  ENV_TEMPLATE: 'env-template',
+  ENV_MODE: 'env-mode',
+  ENV_VALUE: 'env-value',
+}
+
+function getEnvValues(allValues, key) {
+  const toCut = `// @${key}`
+
+  let shouldKeepLooking = true
+  let envValues = []
+  let carriedIndex = 0
+
+  while (shouldKeepLooking) {
+    const firstIndex = allValues.indexOf(toCut, carriedIndex)
+    const lastIndex = allValues.indexOf('// @', firstIndex + 1)
+    const validatedLastIndex = lastIndex !== -1 ? lastIndex : allValues.length
+
+    const newValue = allValues.slice(firstIndex, validatedLastIndex).trim()
+
+    if (newValue) {
+      carriedIndex = validatedLastIndex
+      envValues = [...envValues, newValue]
+    } else {
+      shouldKeepLooking = false
+    }
+  }
+
+  return envValues
+}
+
+function getWebviewContent(fileContent) {
+  const envTemplate = getEnvValues(fileContent, envKeys.ENV_TEMPLATE)
+  const envMode = getEnvValues(fileContent, envKeys.ENV_MODE)
+  const envValue = getEnvValues(fileContent, envKeys.ENV_VALUE)
+
+  const parsedEnvTemplate = parse(envTemplate)
+  const envTemplateHTML = Object.keys(parsedEnvTemplate).map((envKey) => {
+    const value = parsedEnvTemplate[envKey]
+    const formattedValue = `${envKey}: `
+
+    return `
+		<tr>
+				<td>${formattedValue}</td>
+				<td><input type="text" value="${value}"/></td>
+		</tr>
+		`
+  })
+
+  return `
+	<!DOCTYPE html>
+
+	<html lang="en">
+	<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Env Editor</title>
+	</head>
+	<body>
+			<h1>Env Editor</h1>
+			<table>
+					<tr>
+							<th>KEY</th>
+							<th>VALUE</th>
+					</tr>
+					${envTemplateHTML.join('')}
+			</table>
+
+			<p>${envMode.join(' ')}</p>
+
+			<p>${envValue.join(' ')}</p>
+	</body>
+	</html>
+	`
 }
