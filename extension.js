2de9e564bfdd32ce9d9f120d2277d9546466dfbb
olmedoluis <63429352+olmedoluis@users.noreply.github.com>
2021-09-04 22:10:49 -0300

    [FEAT] Make env values take effect (#5)
    * feature:Use array format of file content
    
    * refactor: properly format array groups
    
    * feature: Use array groups to show editable values
    
    * refactor: Remove unused functions
    
    * feature: Use a service to manage env stuff
    
    * feature: Show message when field has been changed
    
    * feature: Set up event with enough information
    
    * refactor: Use right names
    
    * feature: Expect for overwritten data
    
    * refactor: Properly get overwritten stuff
    
    * feature: Let user change between values freely
    
    * feature: Properly declare constants at the top
    
    * refactor: Remove console message
    
    Co-authored-by: olmedoluis <olmedoluis@gmail.com>

--- extension.js
+++ extension.js
@@ -1,21 +1,127 @@
 const vscode = require('vscode')
 const { parse } = require('dotenv')
 
+class EnvironmentHandler {
+  constructor() {
+    this.file = null
+    this.lines = []
+    this.template = {}
+    this.modes = {}
+    this.values = {}
+    this.overwritten = {}
+  }
+
+  setFile(file) {
+    const fileContent = file.document.getText()
+
+    this.file = file
+    this.lines = fileContent.split('\r\n')
+
+    this.readEnvironment()
+  }
+
+  readEnvironment() {
+    const parsedLines = getValuesArray(this.lines)
+
+    parsedLines[envKeys.ENV_MODE] = formatGroupLines(
+      parsedLines[envKeys.ENV_MODE]
+    )
+
+    parsedLines[envKeys.ENV_VALUE] = formatGroupLines(
+      parsedLines[envKeys.ENV_VALUE]
+    )
+
+    this.template = parseEnvTemplate(parsedLines[envKeys.ENV_TEMPLATE])
+    this.overwritten = parseEnvTemplate(parsedLines[envKeys.ENV_OVERWRITTEN])
+    this.modes = parseEnvModes(parsedLines[envKeys.ENV_MODE])
+    this.values = parseEnvValues(parsedLines[envKeys.ENV_VALUE])
+
+    this.overwritten = { ...this.template, ...this.overwritten }
+  }
+
+  updateEnvironment({ envType, envKey, scope, value }) {
+    switch (envType) {
+      case envKeys.ENV_VALUE:
+        this.overwritten[envKey] = value
+        break
+
+      case envKeys.ENV_MODE:
+        this.overwritten = { ...this.overwritten, ...this.modes[scope][value] }
+        break
+    }
+
+    const overwrittenIndex =
+      this.lines.findIndex((line) => line.includes('// @env-overwritten')) + 1
+
+    this.file
+      .edit((editBuilder) => {
+        Object.keys(this.overwritten).forEach((key, index) => {
+          const line = this.file.document.lineAt(overwrittenIndex + index).text
+
+          const linePosition = new vscode.Position(overwrittenIndex + index, 0)
+          const linePositionEnd = new vscode.Position(
+            overwrittenIndex + index,
+            line.length
+          )
+
+          const value = this.overwritten[key]
+
+          editBuilder.replace(
+            new vscode.Range(linePosition, linePositionEnd),
+            `${key}=${value}`
+          )
+        })
+      })
+      .then(() => {
+        this.file.document.save()
+        this.setFile(this.file)
+      })
+  }
+}
+
+const environment = new EnvironmentHandler()
+
+const envKeys = {
+  ENV_TEMPLATE: 'env-template',
+  ENV_MODE: 'env-mode',
+  ENV_VALUE: 'env-value',
+  ENV_OVERWRITTEN: 'env-overwritten',
+}
+
+const eventKeys = {
+  CHANGED_VALUE: 'env-viewer.changedValueEvent',
+  INTERPRETATE_ENVIRONMENT: 'env-viewer.interpretateEnv',
+}
+
+function handleDidReceiveMessage(message) {
+  switch (message.command) {
+    case eventKeys.CHANGED_VALUE:
+      environment.updateEnvironment(message.data)
+  }
+}
+
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
   let interpretateCommand = vscode.commands.registerCommand(
-    'env-viewer.interpretateEnv',
+    eventKeys.INTERPRETATE_ENVIRONMENT,
     function () {
       const panel = vscode.window.createWebviewPanel(
         'editor',
-        'Env Editor',
-        vscode.ViewColumn.Two
+        vscode.window.activeTextEditor.document.fileName,
+        vscode.ViewColumn.Two,
+        { enableScripts: true }
       )
 
-      const fileContent = vscode.window.activeTextEditor.document.getText()
-      panel.webview.html = getWebviewContent(fileContent)
+      environment.setFile(vscode.window.activeTextEditor)
+      panel.webview.html = getWebviewContent()
+
+      panel.webview.onDidReceiveMessage(
+        handleDidReceiveMessage,
+        undefined,
+        context.subscriptions
+      )
 
       vscode.window.showInformationMessage('Interpretating...')
     }
@@ -32,92 +138,119 @@ module.exports = {
   deactivate,
 }
 
-const envKeys = {
-  ENV_TEMPLATE: 'env-template',
-  ENV_MODE: 'env-mode',
-  ENV_VALUE: 'env-value',
-}
+function getValuesArray(lines = []) {
+  let key = envKeys.ENV_TEMPLATE
 
-function getValues(allValues, key) {
-  const toCut = `// @${key}`
+  return lines.reduce((sectionedLines, line) => {
+    const hasNewKey = line.includes('// @')
 
-  let shouldKeepLooking = true
-  let envValues = []
-  let carriedIndex = 0
+    if (hasNewKey) {
+      const newKey = Object.values(envKeys).find((envKey) =>
+        line.includes(envKey)
+      )
 
-  while (shouldKeepLooking) {
-    const firstIndex = allValues.indexOf(toCut, carriedIndex)
-    const lastIndex = allValues.indexOf('// @', firstIndex + 1)
-    const validatedLastIndex = lastIndex !== -1 ? lastIndex : allValues.length
-
-    const newValue = allValues
-      .slice(firstIndex + toCut.length, validatedLastIndex)
-      .trim()
-
-    if (newValue) {
-      carriedIndex = validatedLastIndex
-      envValues = [...envValues, newValue]
-    } else {
-      shouldKeepLooking = false
+      key = newKey || key
     }
-  }
 
-  return envValues
-}
+    const oldLines = sectionedLines[key] || []
 
-const parseEnvValues = (values) => {
-  return values.reduce((valuesAsObjects, value) => {
-    const [formattedValue] = value.match(/\w.+/) || []
-    const parsedValue = parse(formattedValue)
-    const valueSplitted = Object.keys(parsedValue).reduce((values, key) => {
-      const inlineValue = parsedValue[key]
-
-      return { ...values, [key]: inlineValue.split(',') }
-    }, {})
-
-    return { ...valuesAsObjects, ...valueSplitted }
+    return { ...sectionedLines, [key]: [...oldLines, line] }
   }, {})
 }
 
-const parseEnvModes = (modes) => {
-  return modes.reduce((envModes, mode) => {
-    const [modeMetadata] = mode.match(/\w.+/) || ['']
-    const [key, value] = modeMetadata.split('.')
-    const carriedEnvKeyValues = envModes[key] || {}
-    const carriedEnvValues = carriedEnvKeyValues[value] || {}
+function formatGroupLines(lines) {
+  let carriedLines = []
 
-    const envValues = mode
-      .replace(`:${modeMetadata}`, '')
-      .replace(/^\/\//gm, '')
-    const parsedValue = parse(envValues)
+  const formattedLines = lines.reduce((sectionedLines, line) => {
+    const hasNewKey = line.includes('// @')
+
+    if (hasNewKey) {
+      sectionedLines = carriedLines.length
+        ? [...sectionedLines, carriedLines]
+        : sectionedLines
+
+      carriedLines = [line]
+    } else {
+      carriedLines = [...carriedLines, line]
+    }
+
+    return sectionedLines
+  }, [])
+
+  return carriedLines.length
+    ? [...formattedLines, carriedLines]
+    : formattedLines
+}
+
+const parseEnvTemplate = (lines) => {
+  const values = lines.slice(1)
+  const valuesInLine = values.join('\r\n')
+
+  return parse(valuesInLine)
+}
+
+const parseEnvModes = (setOfLines) => {
+  return setOfLines.reduce((envModes, lines) => {
+    const [metadata, ...values] = lines
+    const valuesInLine = values
+      .map((value) => value.replace('//', ''))
+      .join('\r\n')
+
+    const cuttedMetadata = metadata.match(/\/\/ @env-mode:(\w+)\.(\w+)/)
+    const [scope, key] = cuttedMetadata.slice(1)
+
+    const carriedScopes = envModes[scope] || {}
 
     return {
       ...envModes,
-      [key]: {
-        ...carriedEnvKeyValues,
-        [value]: { ...carriedEnvValues, ...parsedValue },
-      },
+      [scope]: { ...carriedScopes, [key]: parse(valuesInLine) },
     }
   }, {})
 }
 
-function getWebviewContent(fileContent) {
-  const envTemplate = getValues(fileContent, envKeys.ENV_TEMPLATE)
-  const envModes = getValues(fileContent, envKeys.ENV_MODE)
-  const envValues = getValues(fileContent, envKeys.ENV_VALUE)
+const parseEnvValues = (setOfLines) => {
+  return setOfLines.reduce((envModes, lines) => {
+    const [metadata, valuesInLine] = lines
+    const values = valuesInLine.replace('//', '').split(',')
 
-  const parsedEnvTemplate = parse(envTemplate)
-  const parsedEnvValues = parseEnvValues(envValues)
-  const parsedEnvModes = parseEnvModes(envModes)
+    const cuttedMetadata = metadata.match(/\/\/ @env-value:(\w+)/)
+    const [key] = cuttedMetadata.slice(1)
 
-  const envTemplateHTML = Object.keys(parsedEnvTemplate).map((envKey) => {
-    const value = parsedEnvTemplate[envKey]
+    return { ...envModes, [key]: values.map((value) => value.trim()) }
+  }, {})
+}
+
+const getEventFunction = ({ envType, envKey = null, scope = null }) => {
+  return `
+  (function() {
+    const value = event.target.value
+    const envType = '${envType}'
+    const envKey = '${envKey}'
+    const scope = '${scope}'
+
+    vscode.postMessage({
+      command: '${eventKeys.CHANGED_VALUE}',
+      data: { envType, envKey, scope, value }
+    })
+  }())
+  `
+}
+
+function getWebviewContent() {
+  const { template, modes, values, overwritten } = environment
+  const realValues = { ...template, ...overwritten }
+
+  const envTemplateHTML = Object.keys(realValues).map((envKey) => {
+    const value = realValues[envKey]
     const formattedValue = `${envKey}: `
-    const hasInputSelect = parsedEnvValues.hasOwnProperty(envKey)
+    const hasInputSelect = values.hasOwnProperty(envKey)
+
+    const eventData = { envType: envKeys.ENV_VALUE, envKey }
+    const commonProps = `onChange="${getEventFunction(eventData)}"`
 
     const input = !hasInputSelect
-      ? `<input type="text" value="${value}"/>`
-      : `<select>${parsedEnvValues[envKey]
+      ? `<input type="text" ${commonProps} value="${value}"/>`
+      : `<select ${commonProps}>${values[envKey]
           .map((option) => `<option value="${option}">${option}</option>`)
           .join('')}</select>`
 
@@ -129,17 +262,19 @@ function getWebviewContent(fileContent) {
 		`
   })
 
-  const envModesHTML = Object.keys(parsedEnvModes).map((envKey) => {
-    const values = parsedEnvModes[envKey]
+  const envModesHTML = Object.keys(modes).map((envKey) => {
+    const values = modes[envKey]
     const formattedValue = `${envKey}: `
     const options = Object.keys(values)
       .map((option) => `<option value="${option}">${option}</option>`)
       .join('')
 
+    const eventData = { envType: envKeys.ENV_MODE, scope: envKey }
+
     return `
 		<tr>
 				<td>${formattedValue}</td>
-				<td><select>${options}</select></td>
+				<td><select onChange="${getEventFunction(eventData)}">${options}</select></td>
 		</tr>
 		`
   })
@@ -175,6 +310,10 @@ function getWebviewContent(fileContent) {
 					</tr>
 					${envTemplateHTML.join('')}
 			</table>
+
+      <script>
+        const vscode = acquireVsCodeApi();
+      </script>
 	</body>
 	</html>
 	`
