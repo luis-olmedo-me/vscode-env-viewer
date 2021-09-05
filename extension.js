2f0021b50ff6403512b2bc2cd3da9488df07366f
olmedoluis <olmedoluis@gmail.com>
2021-09-05 00:01:42 -0300

    refactor: Remove unnessary statement

--- extension.js
+++ extension.js
@@ -128,12 +128,8 @@ function activate(context) {
   context.subscriptions.push(interpretateCommand)
 }
 
-// this method is called when your extension is deactivated
-function deactivate() {}
-
 module.exports = {
   activate,
-  deactivate,
 }
 
 function getValuesArray(lines = []) {
